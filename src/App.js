82647960256fbfe498524f89132d67d5ca34ff2c
Sergei Gorobtsev <sergeigorobtsev@gmail.com>
2022-01-14 18:46:44 +0300

    Feat(post-item):Реализован список постов
    преобразование массива обьектов в массив React елементов

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import {React,useState} from "react";
 import "../src/styles/app.css"
 import Counter from "./components/Сounter";
 import ClassCounter from "./components/СlassCounter"
 import PostItem from "./components/PostItem";
 
 function App() {
+  const [posts,setPosts] = useState( [
+    {id:1, value:"Soup", discription:"Liquid food"},
+    {id:2, value:"Fish", discription:"Food living in liquid"},
+    {id:3, value:"Tea", discription:"Liquid"},
+  ])
   return (
     <div className="App">
       <div className="counters">
         <Counter />
         <ClassCounter />
       </div>
-        <PostItem post={{number:1, value:"Soup", discription:"Liquid food"}} />
-        <PostItem post={{number:2, value:"Fish", discription:"Food living in liquid"}} />
-        <PostItem post={{number:3, value:"Tea", discription:"Liquid"}} />
+      <h2 style={{textAlign:"center"}}>
+        Список постов
+      </h2>
+        {posts.map(post =>
+          <PostItem post={post} key={post.id}/>
+        )}
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -4,7 +4,7 @@ const PostItem = (props) => {
     return (
         <div className='post'>
             <div className='post-content'>
-                <strong>{props.post.number} - {props.post.value}</strong>
+                <strong>{props.post.id} - {props.post.value}</strong>
                 <div> {props.post.discription}</div>
             </div>
             <div className='post-btn'>
